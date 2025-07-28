00570a9cd0731c66aefdca7bffabc0fa48e5713a
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-07-28 17:17:49 +0600

    conditional Statements if, else if and else also learned the logical and and logical or

---
+++ conditionalStatement.js
@@ -0,0 +1,23 @@
+/**
+ * if you buy food for me i will come
+ * if biriyani price is less then 300tk then i will buy
+ * Structure or Formate
+ * if(Condition){ The work will be done here if the condition is true }
+ * else{and if the above if condition fails the else condition will be executed}
+ * 
+ */
+let biriyaniPrice = 200;
+
+if( 13 < 10){
+    console.log('I have a smaller value');
+}
+else{
+    console.log('I have a bigger Value');
+}
+
+if(biriyaniPrice < 300){
+    console.log('I will eat biriyani today.');
+}
+else{
+    console.log('I will not eat biriyani today');
+}
---
+++ multipleCondition.js
@@ -0,0 +1,15 @@
+const PI = 3.1416;
+
+let salary = 18000;
+let isBcs = true;
+const height = 72;
+
+if(salary > 20000 && isBcs && (height > 66)){
+    console.log("Here we go, we got the Supatro");
+}
+else if(((salary >= 20000) && isBcs) || (height > 70)){
+    console.log('Eso Baba Kobul Bolo, biye kore niye jau tomar ordhangini k');
+}
+else{
+    console.log('The Boy is Not GOOD, find Another');
+}
