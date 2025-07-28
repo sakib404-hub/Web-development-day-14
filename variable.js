24fa08bd009a0b451765425ce06081f4765c8a95
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-07-28 16:24:29 +0600

    full recape of the previous module

---
+++ variable.js
@@ -0,0 +1,25 @@
+/*
+*Variable is nothing but a container which the variable will containe
+or store some values
+*Number Type variable
+* String Type Variable
+*Boolean Type Variable
+*/ 
+
+/**(Naming Convensition)
+ * Must be A Single Word
+ * No Quote
+ * No gap
+ * No Dash(Kabeb case which is not allowed in js)
+ * Can not start with a number
+ * Can't be a keyword(Reserved Keyword)
+ * variable should alwyas start with latter, underscore or $(Doller Sign)
+ */
+
+let sunGlassPrice = 50;
+
+let subject = 'CSE';
+
+let flag = true;
+
+let lastExamScore = 87;
